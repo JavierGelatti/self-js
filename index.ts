9f38b27d0f66941fa70c8f8af378811388914852
Facundo Javier Gelatti <javiergelatti@gmail.com>
2024-12-30 01:12:26 -0300

    Actualizar index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import {World} from "./src/world";
+import {Arrow, svgDefinitions} from "./src/arrows";
+
+Arrow.linecapSize = 2;
 
 const world = new World();
 
-document.body.append(world.domElement());
+document.body.append(
+    world.domElement(),
+    svgDefinitions()
+);
 
 world.openOutliner({});
 world.openOutliner({ x: 1, y: 2 });
